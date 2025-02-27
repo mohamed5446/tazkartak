0da7c1c6b151e1325a62ac6011b00206953bd7bb
Ahmed Yahia <a7medya7ia123@gmail.com>
2025-02-28 01:40:50 +0200

    finish user login and register.

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,109 +1,109 @@
+import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router";
-
+import { Link, useNavigate } from "react-router";
+import { useAuthStore } from "../store/authStore";
+import { Loader } from "lucide-react";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 export default function LoginPage() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const onSubmit = (data) => {
+  const navigate = useNavigate();
+  const { isLoading, error, login, setdefaulte } = useAuthStore();
+  const onSubmit = async (data) => {
     console.log(data);
+    try {
+      const response = await login(data);
+      const token = response.token;
+      Cookies.set("token", token, { expires: 7, secure: true });
+      console.log(error);
+      navigate("/");
+    } catch (error) {
+      console.log(error.response.data);
+    }
   };
-
+  useEffect(() => {
+    setdefaulte();
+  }, []);
   return (
-    <div className="flex flex-col items-center">
-      <p className="text-3xl font-bold p-4">
-        <span className="text-blue-500">Taz</span>
-        <span className="text-orange-400">kartk</span>
-      </p>
-      <div className="w-2/3 mx-auto max-w-xl bg-white p-8 shadow-2xl rounded-lg text-end">
-        <form onSubmit={handleSubmit(onSubmit)} className="">
-          <h2 className="text-2xl  font-bold mb-4 text-center">تسجيل الدخول</h2>
+    <div className="size-full flex items-center">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-col items-center w-full"
+      >
+        <p className="text-4xl font-bold p-4">
+          <span className="text-blue-500">Taz</span>
+          <span className="text-orange-400">kartk</span>
+        </p>
+        <div className="w-2/3 mx-auto max-w-xl bg-white p-8 shadow-2xl rounded-lg text-end">
+          <form onSubmit={handleSubmit(onSubmit)} className="">
+            <h2 className="text-2xl text-cyan-dark font-bold mb-4 text-center">
+              تسجيل الدخول
+            </h2>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2">رقم الهاتف</label>
-            <input
-              type="text"
-              {...register("phone", { required: "يرجى إدخال رقم الهاتف" })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.phone && (
-              <p className="text-red-600 text-sm mt-1">
-                {errors.phone.message}
-              </p>
-            )}
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2">كلمة المرور</label>
-            <input
-              type="password"
-              {...register("password", { required: "يرجى إدخال كلمة المرور" })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.password && (
-              <p className="text-red-600 text-sm mt-1">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-
-          <div className="mb-4">
-            <div className="flex justify-end space-x-4 rtl:space-x-reverse">
-              <label className="flex items-center">
-                <input
-                  type="radio"
-                  value="admin"
-                  {...register("accountType", {
-                    required: "يرجى تحديد نوع الحساب",
-                  })}
-                  className="mr-2"
-                />
-                Admin
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">
+                البريد الإلكتروني
               </label>
-              <label className="flex items-center">
-                <input
-                  type="radio"
-                  value="company"
-                  {...register("accountType")}
-                  className="mr-2"
-                />
-                Company
-              </label>
-              <label className="flex items-center">
-                <input
-                  type="radio"
-                  value="user"
-                  {...register("accountType")}
-                  className="mr-2"
-                />
-                User
-              </label>
+              <input
+                type="text"
+                {...register("email", {
+                  required: "يرجى إدخال البريد الالكترونى",
+                })}
+                className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+              />
+              {errors.email && (
+                <p className="text-red-600 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
-            {errors.accountType && (
-              <p className="text-red-600 text-sm mt-1">
-                {errors.accountType.message}
-              </p>
-            )}
-          </div>
 
-          <button
-            type="submit"
-            className="w-full bg-cyan-dark text-white py-2 rounded-lg hover:bg-cyan-900"
-          >
-            تسجيل الدخول
-          </button>
-        </form>
-        <div className="flex flex-row-reverse pt-4">
-          <p className="ml-2">ليس لديك حساب ؟ </p>
-          <Link className="text-blue-600" to={"/signup"}>
-            {" "}
-            انشاء حساب
-          </Link>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">كلمة المرور</label>
+              <input
+                type="password"
+                {...register("password", {
+                  required: "يرجى إدخال كلمة المرور",
+                })}
+                className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+              />
+              {errors.password && (
+                <p className="text-red-600 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            {error && (
+              <p className="text-red-500 font-semibold mt-2">{error}</p>
+            )}
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              type="submit"
+              className="w-full mt-4 bg-cyan-dark text-white py-2 rounded-lg hover:bg-cyan-900"
+            >
+              {isLoading ? (
+                <Loader className="animate-spin mx-auto" size={24} />
+              ) : (
+                " تسجيل الدخول"
+              )}
+            </motion.button>
+          </form>
+          <div className="flex flex-row-reverse pt-4">
+            <p className="ml-2">ليس لديك حساب ؟ </p>
+            <Link className="text-blue-600" to={"/signup"}>
+              {" "}
+              انشاء حساب
+            </Link>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,9 @@
+import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router";
-
+import { Link, useNavigate } from "react-router";
+import { useAuthStore } from "../store/authStore";
+import { Loader } from "lucide-react";
+import { useEffect } from "react";
 export default function SignUpPage() {
   const {
     register,
@@ -8,120 +11,201 @@ export default function SignUpPage() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const {
+    userSignup,
+    adminSignup,
+    companySignup,
+    error,
+    isLoading,
+    setdefaulte,
+  } = useAuthStore();
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const { accountType, ...formData } = data;
+
+    console.log(accountType);
+    try {
+      if (accountType === "user") {
+        await userSignup(formData);
+      } else if (accountType == "company") {
+        await companySignup(formData);
+      }
+      if (accountType == "admin") {
+        await adminSignup(formData);
+      }
+
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error.response);
+    }
   };
-
+  useEffect(() => {
+    setdefaulte();
+  }, []);
   return (
-    <div className="w-2/3 mx-auto max-w-xl bg-white p-8 shadow-2xl rounded-lg text-end m-4">
-      <h2 className="text-2xl text-cyan-dark font-bold mb-6 text-center">
-        إنشاء حساب
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <label className="block text-gray-700 mb-2">اسم</label>
-            <input
-              type="text"
-              {...register("name", { required: "يرجى إدخال الاسم" })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.name && (
-              <p className="text-red-600 text-sm">{errors.name.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-gray-700 mb-2">رقم الهاتف</label>
-            <input
-              type="text"
-              {...register("phone", { required: "يرجى إدخال رقم الهاتف" })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.phone && (
-              <p className="text-red-600 text-sm">{errors.phone.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-gray-700 mb-2">
-              البريد الإلكتروني
-            </label>
-            <input
-              type="email"
-              {...register("email", {
-                required: "يرجى إدخال البريد الإلكتروني",
-              })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.email && (
-              <p className="text-red-600 text-sm">{errors.email.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-gray-700 mb-2">كلمة المرور</label>
-            <input
-              type="password"
-              {...register("password", { required: "يرجى إدخال كلمة المرور" })}
-              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
-            />
-            {errors.password && (
-              <p className="text-red-600 text-sm">{errors.password.message}</p>
-            )}
-          </div>
-        </div>
-
-        <div className="mt-4">
-          <div className="flex space-x-4 flex-row-reverse">
-            <label className="flex items-center">
-              <input
-                type="radio"
-                value="admin"
-                {...register("accountType", {
-                  required: "يرجى تحديد نوع الحساب",
-                })}
-                className="mr-2"
-              />
-              Admin
-            </label>
-            <label className="flex items-center">
-              <input
-                type="radio"
-                value="company"
-                {...register("accountType")}
-                className="mr-2"
-              />
-              Company
-            </label>
-            <label className="flex items-center mx-2">
-              <input
-                type="radio"
-                value="user"
-                {...register("accountType")}
-                className="mr-2"
-              />
-              User
-            </label>
-          </div>
-          {errors.accountType && (
-            <p className="text-red-600 text-sm">{errors.accountType.message}</p>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          className="w-full mt-6 bg-cyan-dark text-white py-2 rounded-lg hover:bg-cyan-900"
-        >
-          إنشاء حساب
-        </button>
-        <p className="text-center mt-4">
-          هل لديك حساب؟{" "}
-          <Link to={"/login"} className="text-blue-600 ">
-            تسجيل الدخول
-          </Link>
+    <div className="size-full flex  ">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full m-auto"
+      >
+        <p className="text-4xl font-bold text-center p-4">
+          <span className="text-blue-500">Taz</span>
+          <span className="text-orange-400">kartk</span>
         </p>
-      </form>
+        <div className="w-2/3 mx-auto max-w-xl bg-white p-8 shadow-2xl rounded-lg text-end m-4">
+          <h2 className="text-2xl text-cyan-dark font-bold mb-6 text-center">
+            إنشاء حساب
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <label className="block text-gray-700 mb-2">اسم العائلة </label>
+                <input
+                  type="text"
+                  {...register("lastName", {
+                    required: "يرجى إدخال اسم العائلة",
+                  })}
+                  className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+                />
+                {errors.lastName && (
+                  <p className="text-red-600 text-sm">
+                    {errors.lastName.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label className="block text-gray-700 mb-2">الاسم الاول</label>
+                <input
+                  type="text"
+                  {...register("firstName", {
+                    required: "يرجى إدخال الاسم الاول",
+                  })}
+                  className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+                />
+                {errors.firstName && (
+                  <p className="text-red-600 text-sm">
+                    {errors.firstName.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <label className="block text-gray-700 mb-2">رقم الهاتف</label>
+                <input
+                  type="text"
+                  {...register("phoneNumber", {
+                    required: "يرجى إدخال رقم الهاتف",
+                  })}
+                  className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+                />
+                {errors.phoneNumber && (
+                  <p className="text-red-600 text-sm">
+                    {errors.phoneNumber.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <label className="block text-gray-700 mb-2">
+                  البريد الإلكتروني
+                </label>
+                <input
+                  type="email"
+                  {...register("email", {
+                    required: "يرجى إدخال البريد الإلكتروني",
+                  })}
+                  className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+                />
+                {errors.email && (
+                  <p className="text-red-600 text-sm">{errors.email.message}</p>
+                )}
+              </div>
+
+              <div className="col-span-2">
+                <label className="block text-gray-700 mb-2">كلمة المرور</label>
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: "يرجى إدخال كلمة المرور",
+                  })}
+                  className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-700"
+                />
+                {errors.password && (
+                  <p className="text-red-600 text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <div className="flex space-x-4 flex-row-reverse">
+                <label className="flex items-center">
+                  <input
+                    type="radio"
+                    value="admin"
+                    {...register("accountType", {
+                      required: "يرجى تحديد نوع الحساب",
+                    })}
+                    className="mr-2"
+                  />
+                  Admin
+                </label>
+                <label className="flex items-center">
+                  <input
+                    type="radio"
+                    value="company"
+                    {...register("accountType")}
+                    className="mr-2"
+                  />
+                  Company
+                </label>
+                <label className="flex items-center mx-2">
+                  <input
+                    type="radio"
+                    value="user"
+                    {...register("accountType")}
+                    className="mr-2"
+                  />
+                  User
+                </label>
+              </div>
+              {errors.accountType && (
+                <p className="text-red-600 text-sm">
+                  {errors.accountType.message}
+                </p>
+              )}
+            </div>
+            <div>
+              {error && (
+                <p className="text-red-500 font-semibold mt-2">{error}</p>
+              )}
+            </div>
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              type="submit"
+              className="w-full mt-4 bg-cyan-dark text-white py-2 rounded-lg hover:bg-cyan-900"
+            >
+              {isLoading ? (
+                <Loader className="animate-spin mx-auto" size={24} />
+              ) : (
+                " إنشاء حساب"
+              )}
+            </motion.button>
+            <p className="text-center mt-4">
+              هل لديك حساب؟{" "}
+              <Link to={"/login"} className="text-blue-600 ">
+                تسجيل الدخول
+              </Link>
+            </p>
+          </form>
+        </div>
+      </motion.div>
     </div>
   );
 }
