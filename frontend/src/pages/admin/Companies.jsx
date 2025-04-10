c872d0faf036673de1d68508e4f4b83cc879856f
Ahmed Yahia <a7medya7ia123@gmail.com>
2025-04-10 19:38:49 +0200

    fix bug

--- frontend/src/pages/admin/Companies.jsx
+++ frontend/src/pages/admin/Companies.jsx
@@ -313,8 +313,8 @@ export default function Companies() {
                 {...register2("phone", {
                   required: "رقم الهاتف مطلوب",
                   pattern: {
-                    value: /^[0-9]{10,11}$/,
-                    message: "يجب أن يكون رقم الهاتف من 10 إلى 11 رقماً",
+                    value: /^[0-9]{11}$/,
+                    message: "يجب أن يكون رقم الهاتف 11 رقماً ",
                   },
                 })}
                 className="w-full border p-2 my-2 rounded "
@@ -544,8 +544,8 @@ export default function Companies() {
                 {...register("phone", {
                   required: "رقم الهاتف مطلوب",
                   pattern: {
-                    value: /^[0-9]{10,11}$/,
-                    message: "يجب أن يكون رقم الهاتف من 10 إلى 11 رقماً",
+                    value: /^[0-9]{11}$/,
+                    message: "يجب أن يكون رقم الهاتف 11 رقماً ",
                   },
                 })}
                 className="w-full border p-2 my-2 rounded "
